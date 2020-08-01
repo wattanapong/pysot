11c6ec9bab7e676ddfad80c05497676e10249510
Wattanapong Suttapak <wattanapong.su@up.ac.th>
2020-08-02 00:59:24 +0700

    add(minus from prediction location) crop shifted position  in l1 loss

--- pysot/tracker/siamrpn_attack_2pass.py
+++ pysot/tracker/siamrpn_attack_2pass.py
@@ -112,7 +112,7 @@ class SiamRPNAttack2Pass(SiameseTracker):
         x, y, w, h = pred_box
         wa = torch.tensor(self.size[0]).cuda() * (1 - lr) + wa * lr
         ha = torch.tensor(self.size[1]).cuda() * (1 - lr) + ha * lr
-        c_loss = -1 * torch.sum(torch.sqrt((xa - self.shift[0, idx]) **2 +(ya - self.shift[1, idx])**2))
+        c_loss = -1 * torch.sum(torch.sqrt((xa + self.shift[0, idx]) **2 +(ya + self.shift[1, idx])**2))
         # shape_loss = -1 * torch.sum(torch.sqrt((wa - w)**2+(ha - h)**2))
         # return c_loss + shape_loss
         return c_loss
